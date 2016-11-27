d10c917f78ce0e6e4f0b09b2d9b2919cbb965a3a
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2016-11-27 14:26:39 -0800

    Plotting with titles/labels and to file

---
+++ plot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import matplotlib.pyplot as plt
+
+def main():
+	xs = []
+	ys = []
+	csv_filename = sys.argv[1]
+	plt_title = sys.argv[2]
+	png_filename = sys.argv[3] if len(sys.argv) > 3 else None
+	with open(csv_filename) as csv_file:
+		reader = csv.reader(csv_file, delimiter=',')
+		for row in reader:
+			assert(len(row) == 2)
+			xs.append(row[0])
+			ys.append(row[1])
+	
+	plt.plot(xs, ys, 'ro')
+	plt.title(plt_title)
+	plt.xlabel('x1')
+	plt.ylabel('x2')
+	plt.axis([-1, 1, -1, 1])
+	plt.grid(True)
+	if png_filename is not None:
+		plt.savefig(png_filename)
+	else:
+		plt.show()
+
+if __name__ == "__main__":
+	main()
